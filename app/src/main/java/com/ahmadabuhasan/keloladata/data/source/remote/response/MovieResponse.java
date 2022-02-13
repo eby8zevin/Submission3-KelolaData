1601f9f35a18079e384617c6505fcd15f81e87b0
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-13 22:07:13 +0700

    Submission 3

---
+++ app/src/main/java/com/ahmadabuhasan/keloladata/data/source/remote/response/MovieResponse.java
@@ -0,0 +1,107 @@
+package com.ahmadabuhasan.keloladata.data.source.remote.response;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class MovieResponse implements Parcelable {
+
+    private String movieId;
+    private String overview;
+    private String posterPath;
+    private String releaseDate;
+    private String title;
+    private String voteAverage;
+
+    public MovieResponse(String movieId, String overview, String posterPath, String releaseDate, String title, String voteAverage) {
+        this.movieId = movieId;
+        this.overview = overview;
+        this.posterPath = posterPath;
+        this.releaseDate = releaseDate;
+        this.title = title;
+        this.voteAverage = voteAverage;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(String voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    protected MovieResponse(Parcel in) {
+        movieId = in.readString();
+        overview = in.readString();
+        posterPath = in.readString();
+        releaseDate = in.readString();
+        title = in.readString();
+        voteAverage = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(movieId);
+        dest.writeString(overview);
+        dest.writeString(posterPath);
+        dest.writeString(releaseDate);
+        dest.writeString(title);
+        dest.writeString(voteAverage);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<MovieResponse> CREATOR = new Creator<MovieResponse>() {
+        @Override
+        public MovieResponse createFromParcel(Parcel in) {
+            return new MovieResponse(in);
+        }
+
+        @Override
+        public MovieResponse[] newArray(int size) {
+            return new MovieResponse[size];
+        }
+    };
+}
