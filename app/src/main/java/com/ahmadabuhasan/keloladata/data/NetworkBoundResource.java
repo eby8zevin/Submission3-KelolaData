2b169f6e395e125740da28dfac2612d2c20bc1f1
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-14 00:15:17 +0700

    Submission 3

--- app/src/main/java/com/ahmadabuhasan/keloladata/data/NetworkBoundResource.java
+++ app/src/main/java/com/ahmadabuhasan/keloladata/data/NetworkBoundResource.java
@@ -9,8 +9,8 @@ import com.ahmadabuhasan.keloladata.vo.Resource;
 
 public abstract class NetworkBoundResource<ResultType, RequestType> {
 
-    private MediatorLiveData<Resource<ResultType>> result = new MediatorLiveData<>();
-    private AppExecutors appExecutors;
+    private final MediatorLiveData<Resource<ResultType>> result = new MediatorLiveData<>();
+    private final AppExecutors appExecutors;
 
     public NetworkBoundResource(AppExecutors mAppExecutors) {
         this.appExecutors = mAppExecutors;
