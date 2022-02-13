9b511c1cab6f37e46d64e20319ad240d6e643a3a
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-13 22:04:07 +0700

    Submission 3

---
+++ app/src/main/java/com/ahmadabuhasan/keloladata/data/AppDataSource.java
@@ -0,0 +1,14 @@
+package com.ahmadabuhasan.keloladata.data;
+
+import androidx.lifecycle.LiveData;
+import androidx.paging.PagedList;
+
+import com.ahmadabuhasan.keloladata.data.source.local.entity.MovieEntity;
+import com.ahmadabuhasan.keloladata.vo.Resource;
+
+public interface AppDataSource {
+
+    LiveData<Resource<PagedList<MovieEntity>>> getAllMovies();
+
+    //LiveData<Resource<MovieEntity>> getDetailMovie(String movieId);
+}
