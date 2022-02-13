695b8faa8586eb4f78d8285554c51e541b09a248
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-13 22:03:45 +0700

    Submission 3

---
+++ app/src/main/java/com/ahmadabuhasan/keloladata/data/source/local/room/AppDao.java
@@ -0,0 +1,31 @@
+package com.ahmadabuhasan.keloladata.data.source.local.room;
+
+import androidx.lifecycle.LiveData;
+import androidx.paging.DataSource;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Transaction;
+import androidx.room.Update;
+
+import com.ahmadabuhasan.keloladata.data.source.local.entity.MovieEntity;
+
+import java.util.List;
+
+@Dao
+public interface AppDao {
+
+    @Query("SELECT * FROM movieTable")
+    DataSource.Factory<Integer, MovieEntity> getMovies();
+
+    @Transaction
+    @Query("SELECT * FROM movieTable WHERE movieId = :id")
+    LiveData<MovieEntity> getMovieById(String id);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertMovies(List<MovieEntity> movies);
+
+    @Update
+    void updateMovies(MovieEntity movies);
+}
