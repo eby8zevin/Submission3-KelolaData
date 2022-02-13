4b984e562f432e23a01aa707022b1cd3f3263f86
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-13 22:06:27 +0700

    Submission 3

--- app/src/main/java/com/ahmadabuhasan/keloladata/MainActivity.java
+++ app/src/main/java/com/ahmadabuhasan/keloladata/MainActivity.java
@@ -1,14 +1,28 @@
 package com.ahmadabuhasan.keloladata;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.ahmadabuhasan.keloladata.databinding.ActivityMainBinding;
+import com.ahmadabuhasan.keloladata.ui.home.HomeActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        new Handler().postDelayed(() -> {
+            Intent i = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(i);
+            finish();
+        }, 3000);
     }
 }
