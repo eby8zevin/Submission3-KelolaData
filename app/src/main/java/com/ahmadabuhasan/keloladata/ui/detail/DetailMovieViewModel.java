dbcd31ae2126eae0eb91b9390a67bc91f51ee178
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-14 09:30:16 +0700

    Submission 3

---
+++ app/src/main/java/com/ahmadabuhasan/keloladata/ui/detail/DetailMovieViewModel.java
@@ -0,0 +1,46 @@
+package com.ahmadabuhasan.keloladata.ui.detail;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
+import androidx.lifecycle.ViewModel;
+
+import com.ahmadabuhasan.keloladata.data.AppRepository;
+import com.ahmadabuhasan.keloladata.data.source.local.entity.MovieEmbedded;
+import com.ahmadabuhasan.keloladata.data.source.local.entity.MovieEntity;
+import com.ahmadabuhasan.keloladata.vo.Resource;
+
+public class DetailMovieViewModel extends ViewModel {
+
+    private MutableLiveData<String> movieId = new MutableLiveData<>();
+    private AppRepository repository;
+
+    public DetailMovieViewModel(AppRepository mRepository) {
+        this.repository = mRepository;
+    }
+
+    public LiveData<Resource<MovieEmbedded>> movieEmbedded = Transformations.switchMap(movieId,
+            mMoviesId -> repository.getDetailMovie(mMoviesId));
+
+    public String getMovieId() {
+        return movieId.getValue();
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId.setValue(movieId);
+    }
+
+    void setLike() {
+        Resource<MovieEmbedded> moduleResource = movieEmbedded.getValue();
+        if (moduleResource != null) {
+            MovieEmbedded movieEmbedded = moduleResource.data;
+
+            if (movieEmbedded != null) {
+                MovieEntity movieEntity = movieEmbedded.mMovies;
+
+                final boolean newState = !movieEntity.isLiked();
+                repository.setMovieLike(movieEntity, newState);
+            }
+        }
+    }
+}
