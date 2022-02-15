d1a271c0b942bd886dcabea374874a6c8fafc34b
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-15 22:07:37 +0700

    Submission 3

---
+++ app/src/main/java/com/ahmadabuhasan/keloladata/ui/favorite/tvshow/FavoriteTVShowFragment.java
@@ -0,0 +1,66 @@
+package com.ahmadabuhasan.keloladata.ui.favorite.tvshow;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ahmadabuhasan.keloladata.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FavoriteTVShowFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FavoriteTVShowFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public FavoriteTVShowFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FavoriteTVShowFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FavoriteTVShowFragment newInstance(String param1, String param2) {
+        FavoriteTVShowFragment fragment = new FavoriteTVShowFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorite_tvshow, container, false);
+    }
+}
