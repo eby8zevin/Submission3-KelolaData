9f244e98996d6a751acbd780ac9ba4ce6b1736c1
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-15 22:08:19 +0700

    Submission 3

--- app/src/main/java/com/ahmadabuhasan/keloladata/ui/favorite/movie/FavoriteMovieAdapter.java
+++ app/src/main/java/com/ahmadabuhasan/keloladata/ui/favorite/movie/FavoriteMovieAdapter.java
@@ -1,4 +1,4 @@
-package com.ahmadabuhasan.keloladata.ui.favorite;
+package com.ahmadabuhasan.keloladata.ui.favorite.movie;
 
 import android.annotation.SuppressLint;
 import android.view.LayoutInflater;
@@ -10,12 +10,12 @@ import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.ahmadabuhasan.keloladata.data.source.local.entity.MovieEntity;
-import com.ahmadabuhasan.keloladata.databinding.ItemFavoriteBinding;
+import com.ahmadabuhasan.keloladata.databinding.ItemListBinding;
 import com.bumptech.glide.Glide;
 
-public class FavMovieAdapter extends PagedListAdapter<MovieEntity, FavMovieAdapter.FavoriteViewHolder> {
+public class FavoriteMovieAdapter extends PagedListAdapter<MovieEntity, FavoriteMovieAdapter.FavoriteViewHolder> {
 
-    FavMovieAdapter() {
+    FavoriteMovieAdapter() {
         super(DIFF_CALLBACK);
     }
 
@@ -35,13 +35,13 @@ public class FavMovieAdapter extends PagedListAdapter<MovieEntity, FavMovieAdapt
 
     @NonNull
     @Override
-    public FavMovieAdapter.FavoriteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        ItemFavoriteBinding binding = ItemFavoriteBinding.inflate(LayoutInflater.from(parent.getContext()));
+    public FavoriteMovieAdapter.FavoriteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemListBinding binding = ItemListBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
         return new FavoriteViewHolder(binding);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull FavMovieAdapter.FavoriteViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull FavoriteMovieAdapter.FavoriteViewHolder holder, int position) {
         MovieEntity movie = getItem(position);
         if (movie != null) {
             holder.bind(movie);
@@ -49,20 +49,19 @@ public class FavMovieAdapter extends PagedListAdapter<MovieEntity, FavMovieAdapt
     }
 
     static class FavoriteViewHolder extends RecyclerView.ViewHolder {
-        final ItemFavoriteBinding binding;
+        final ItemListBinding binding;
 
-        FavoriteViewHolder(ItemFavoriteBinding binding) {
+        FavoriteViewHolder(ItemListBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
         }
 
         void bind(MovieEntity movie) {
-            binding.tvTitle.setText(movie.getTitle());
-            binding.tvDate.setText(movie.getReleaseDate());
-
             Glide.with(itemView.getContext())
                     .load(movie.getPosterPath())
                     .into(binding.imgPoster);
+            binding.tvTitle.setText(movie.getTitle());
+            binding.tvDate.setText(movie.getReleaseDate());
         }
     }
 }
