865c7739d89eccb6a4b8bcc615285765211ad410
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-16 21:10:06 +0700

    Submission 3

---
+++ app/src/main/java/com/ahmadabuhasan/keloladata/ui/detail/DetailTVShowActivity.java
@@ -0,0 +1,16 @@
+package com.ahmadabuhasan.keloladata.ui.detail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ahmadabuhasan.keloladata.R;
+
+public class DetailTVShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_tvshow);
+    }
+}
