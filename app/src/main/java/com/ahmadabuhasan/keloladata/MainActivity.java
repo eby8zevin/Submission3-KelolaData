b100f695684fd8182dc09abe6f348c4ca18c58ac
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-17 23:28:48 +0700

    Submission 3

--- app/src/main/java/com/ahmadabuhasan/keloladata/MainActivity.java
+++ app/src/main/java/com/ahmadabuhasan/keloladata/MainActivity.java
@@ -3,11 +3,15 @@ package com.ahmadabuhasan.keloladata;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.ahmadabuhasan.keloladata.databinding.ActivityMainBinding;
 import com.ahmadabuhasan.keloladata.ui.home.HomeActivity;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +20,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
         new Handler().postDelayed(() -> {
             Intent i = new Intent(MainActivity.this, HomeActivity.class);
