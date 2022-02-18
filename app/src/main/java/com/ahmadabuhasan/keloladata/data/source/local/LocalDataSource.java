1b7d52f69582e834451751102fa5074e344116ae
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-19 06:29:21 +0700

    Submission 3

--- app/src/main/java/com/ahmadabuhasan/keloladata/data/source/local/LocalDataSource.java
+++ app/src/main/java/com/ahmadabuhasan/keloladata/data/source/local/LocalDataSource.java
@@ -5,6 +5,8 @@ import androidx.paging.DataSource;
 
 import com.ahmadabuhasan.keloladata.data.source.local.entity.MovieEmbedded;
 import com.ahmadabuhasan.keloladata.data.source.local.entity.MovieEntity;
+import com.ahmadabuhasan.keloladata.data.source.local.entity.TVShowEmbedded;
+import com.ahmadabuhasan.keloladata.data.source.local.entity.TVShowEntity;
 import com.ahmadabuhasan.keloladata.data.source.local.room.AppDao;
 
 import java.util.List;
@@ -25,12 +27,17 @@ public class LocalDataSource {
         return INSTANCE;
     }
 
+    // Movie
     public DataSource.Factory<Integer, MovieEntity> getAllMovies() {
         return appDao.getMovies();
     }
 
     public LiveData<MovieEmbedded> getMoviesById(String movieId) {
         return appDao.getMovieById(movieId);
+    }
+
+    public void insertMovies(List<MovieEntity> movies) {
+        appDao.insertMovies(movies);
     }
 
     public DataSource.Factory<Integer, MovieEntity> getLikedMovies() {
@@ -42,7 +49,25 @@ public class LocalDataSource {
         appDao.updateMovie(movie);
     }
 
-    public void insertMovies(List<MovieEntity> movies) {
-        appDao.insertMovies(movies);
+    // TV Show
+    public DataSource.Factory<Integer, TVShowEntity> getAllTVShows() {
+        return appDao.getTVShows();
+    }
+
+    public LiveData<TVShowEmbedded> getTVShowsById(String tvShowId) {
+        return appDao.getTVShowById(tvShowId);
+    }
+
+    public void insertTVShows(List<TVShowEntity> tvShows) {
+        appDao.insertTVShows(tvShows);
+    }
+
+    public DataSource.Factory<Integer, TVShowEntity> getLikedTVShows() {
+        return appDao.getLikedTVShow();
+    }
+
+    public void setTVShowLike(TVShowEntity tvShow, boolean newState) {
+        tvShow.setLiked(newState);
+        appDao.updateTVShow(tvShow);
     }
 }
