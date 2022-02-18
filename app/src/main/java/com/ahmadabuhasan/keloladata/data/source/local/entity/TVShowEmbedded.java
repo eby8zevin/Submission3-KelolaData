da033840094784408bd45e1df0d6f12ff69f1c44
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-19 06:30:02 +0700

    Submission 3

---
+++ app/src/main/java/com/ahmadabuhasan/keloladata/data/source/local/entity/TVShowEmbedded.java
@@ -0,0 +1,9 @@
+package com.ahmadabuhasan.keloladata.data.source.local.entity;
+
+import androidx.room.Embedded;
+
+public class TVShowEmbedded {
+
+    @Embedded
+    public TVShowEntity mTVShows;
+}
