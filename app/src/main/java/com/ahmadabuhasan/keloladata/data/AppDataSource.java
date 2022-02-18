dfaf211c49d8afa5a9840bda841c1776ef9130bd
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-19 06:28:14 +0700

    Submission 3

--- app/src/main/java/com/ahmadabuhasan/keloladata/data/AppDataSource.java
+++ app/src/main/java/com/ahmadabuhasan/keloladata/data/AppDataSource.java
@@ -5,10 +5,13 @@ import androidx.paging.PagedList;
 
 import com.ahmadabuhasan.keloladata.data.source.local.entity.MovieEmbedded;
 import com.ahmadabuhasan.keloladata.data.source.local.entity.MovieEntity;
+import com.ahmadabuhasan.keloladata.data.source.local.entity.TVShowEmbedded;
+import com.ahmadabuhasan.keloladata.data.source.local.entity.TVShowEntity;
 import com.ahmadabuhasan.keloladata.vo.Resource;
 
 public interface AppDataSource {
 
+    // Movie
     LiveData<Resource<PagedList<MovieEntity>>> getAllMovies();
 
     LiveData<Resource<MovieEmbedded>> getDetailMovie(String movieId);
@@ -16,4 +19,13 @@ public interface AppDataSource {
     LiveData<PagedList<MovieEntity>> getLikedMovies();
 
     void setMovieLike(MovieEntity movie, boolean state);
+
+    // TV Show
+    LiveData<Resource<PagedList<TVShowEntity>>> getAllTVShows();
+
+    //LiveData<Resource<TVShowEmbedded>> getDetailTVShow(String tvShowId);
+
+    //LiveData<PagedList<TVShowEntity>> getLikedTVShows();
+
+    //void setTVShowLike(TVShowEntity tvShow, boolean state);
 }
