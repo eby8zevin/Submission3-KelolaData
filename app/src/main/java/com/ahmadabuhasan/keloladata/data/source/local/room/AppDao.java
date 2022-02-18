aa47572dfa28cc946bb44d6ca038b61c6896b8d8
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-19 06:28:08 +0700

    Submission 3

--- app/src/main/java/com/ahmadabuhasan/keloladata/data/source/local/room/AppDao.java
+++ app/src/main/java/com/ahmadabuhasan/keloladata/data/source/local/room/AppDao.java
@@ -11,25 +11,45 @@ import androidx.room.Update;
 
 import com.ahmadabuhasan.keloladata.data.source.local.entity.MovieEmbedded;
 import com.ahmadabuhasan.keloladata.data.source.local.entity.MovieEntity;
+import com.ahmadabuhasan.keloladata.data.source.local.entity.TVShowEmbedded;
+import com.ahmadabuhasan.keloladata.data.source.local.entity.TVShowEntity;
 
 import java.util.List;
 
 @Dao
 public interface AppDao {
 
+    // Movie
     @Query("SELECT * FROM movieTable")
     DataSource.Factory<Integer, MovieEntity> getMovies();
 
-    @Query("SELECT * FROM movieTable WHERE liked = 1")
-    DataSource.Factory<Integer, MovieEntity> getLikedMovie();
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertMovies(List<MovieEntity> movies);
 
     @Transaction
     @Query("SELECT * FROM movieTable WHERE movieId = :id")
     LiveData<MovieEmbedded> getMovieById(String id);
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    void insertMovies(List<MovieEntity> movies);
+    @Query("SELECT * FROM movieTable WHERE liked = 1")
+    DataSource.Factory<Integer, MovieEntity> getLikedMovie();
 
     @Update
     void updateMovie(MovieEntity movies);
+
+    // TV Show
+    @Query("SELECT * FROM tvShowTable")
+    DataSource.Factory<Integer, TVShowEntity> getTVShows();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertTVShows(List<TVShowEntity> tvShows);
+
+    @Transaction
+    @Query("SELECT * FROM tvShowTable WHERE tvShowId = :id")
+    LiveData<TVShowEmbedded> getTVShowById(String id);
+
+    @Query("SELECT * FROM tvShowTable WHERE liked = 1")
+    DataSource.Factory<Integer, TVShowEntity> getLikedTVShow();
+
+    @Update
+    void updateTVShow(TVShowEntity tvShows);
 }
