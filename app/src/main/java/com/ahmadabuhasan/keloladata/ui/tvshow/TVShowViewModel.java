db78002a9042e30e0e5a9e8b7101999dd54f42e3
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-19 06:30:26 +0700

    Submission 3

--- app/src/main/java/com/ahmadabuhasan/keloladata/ui/tvshow/TVShowViewModel.java
+++ app/src/main/java/com/ahmadabuhasan/keloladata/ui/tvshow/TVShowViewModel.java
@@ -1,4 +1,22 @@
 package com.ahmadabuhasan.keloladata.ui.tvshow;
 
-public class TVShowViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+import androidx.paging.PagedList;
+
+import com.ahmadabuhasan.keloladata.data.AppRepository;
+import com.ahmadabuhasan.keloladata.data.source.local.entity.TVShowEntity;
+import com.ahmadabuhasan.keloladata.vo.Resource;
+
+public class TVShowViewModel extends ViewModel {
+
+    private final AppRepository repository;
+
+    public TVShowViewModel(AppRepository mRepository) {
+        this.repository = mRepository;
+    }
+
+    public LiveData<Resource<PagedList<TVShowEntity>>> getTVShows() {
+        return repository.getAllTVShows();
+    }
 }
