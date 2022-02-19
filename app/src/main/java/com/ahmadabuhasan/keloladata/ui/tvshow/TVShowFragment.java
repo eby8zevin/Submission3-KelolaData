6f5ccc0164a82569ff0aa8c582b3604b4eed50f3
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-19 07:05:04 +0700

    Submission 3

--- app/src/main/java/com/ahmadabuhasan/keloladata/ui/tvshow/TVShowFragment.java
+++ app/src/main/java/com/ahmadabuhasan/keloladata/ui/tvshow/TVShowFragment.java
@@ -1,17 +1,19 @@
 package com.ahmadabuhasan.keloladata.ui.tvshow;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.ahmadabuhasan.keloladata.R;
 import com.ahmadabuhasan.keloladata.databinding.FragmentTvshowBinding;
+import com.ahmadabuhasan.keloladata.viewmodel.ViewModelFactory;
 
 public class TVShowFragment extends Fragment {
 
@@ -22,7 +24,7 @@ public class TVShowFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentTvshowBinding.inflate(inflater);
@@ -33,6 +35,38 @@ public class TVShowFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        if (getActivity() != null) {
+            ViewModelFactory factory = ViewModelFactory.getInstance(getActivity());
+            TVShowViewModel viewModel = new ViewModelProvider(this, factory).get(TVShowViewModel.class);
 
+            TVShowAdapter adapter = new TVShowAdapter();
+            viewModel.getTVShows().observe(this, tvShows -> {
+                if (tvShows != null) {
+                    switch (tvShows.status) {
+                        case LOADING:
+                            binding.progressBar.setVisibility(View.VISIBLE);
+                            break;
+                        case SUCCESS:
+                            binding.progressBar.setVisibility(View.GONE);
+                            adapter.submitList(tvShows.data);
+                            break;
+                        case ERROR:
+                            binding.progressBar.setVisibility(View.GONE);
+                            Toast.makeText(getContext(), "There is an error", Toast.LENGTH_SHORT).show();
+                            break;
+                    }
+                }
+            });
+
+            binding.rvTvShow.setLayoutManager(new LinearLayoutManager(getContext()));
+            binding.rvTvShow.setHasFixedSize(true);
+            binding.rvTvShow.setAdapter(adapter);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 }
