caddf7c3324e1bc66fd1e20d9b74d48eac41720e
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-19 18:04:51 +0700

    Submission 3

--- app/src/main/java/com/ahmadabuhasan/keloladata/ui/favorite/tvshow/FavoriteTVShowViewModel.java
+++ app/src/main/java/com/ahmadabuhasan/keloladata/ui/favorite/tvshow/FavoriteTVShowViewModel.java
@@ -1,4 +1,26 @@
 package com.ahmadabuhasan.keloladata.ui.favorite.tvshow;
 
-public class FavoriteTVShowViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+import androidx.paging.PagedList;
+
+import com.ahmadabuhasan.keloladata.data.AppRepository;
+import com.ahmadabuhasan.keloladata.data.source.local.entity.TVShowEntity;
+
+public class FavoriteTVShowViewModel extends ViewModel {
+
+    private final AppRepository repository;
+
+    public FavoriteTVShowViewModel(AppRepository mRepository) {
+        this.repository = mRepository;
+    }
+
+    public LiveData<PagedList<TVShowEntity>> getLikes() {
+        return repository.getLikedTVShows();
+    }
+
+    void setLike(TVShowEntity tvShowEntity) {
+        final boolean newState = !tvShowEntity.isLiked();
+        repository.setTVShowLike(tvShowEntity, newState);
+    }
 }
